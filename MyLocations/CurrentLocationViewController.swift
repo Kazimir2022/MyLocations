e19ea5af9f67ca6dabb9f079af9d12f33318569b
Kazimir Makaranka <m2014ky@gmail.com>
2023-07-16 12:47:48 +0300

    Get your current location. Ask for permission

--- MyLocations/CurrentLocationViewController.swift
+++ MyLocations/CurrentLocationViewController.swift
@@ -6,14 +6,17 @@
 //
 
 import UIKit
+import CoreLocation
 
-class CurrentLocationViewController: UIViewController {
+class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate {
   @IBOutlet weak var messageLabel: UILabel!
   @IBOutlet weak var latitudeLabel: UILabel!
   @IBOutlet weak var longitudeLabel: UILabel!
   @IBOutlet weak var addressLabel: UILabel!
   @IBOutlet weak var tagButton: UIButton!
   @IBOutlet weak var getButton: UIButton!
+  
+  let locationManager = CLLocationManager()
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -22,8 +25,31 @@ class CurrentLocationViewController: UIViewController {
   
   // MARK: - Actions
   @IBAction func getLocation() {
+    let authStatus = locationManager.authorizationStatus
+    if authStatus == .notDetermined {
+      locationManager.requestWhenInUseAuthorization()
+      return
+    }
     
+    locationManager.delegate = self
+    locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+    locationManager.startUpdatingLocation()
   }
   
+  // MARK: - CLLocationManagerDelegate
+  func locationManager(
+    _ manager: CLLocationManager,
+    didFailWithError error: Error
+  ) {
+    print("didFailWithError \(error.localizedDescription)")
+  }
+  
+  func locationManager(
+    _ manager: CLLocationManager,
+    didUpdateLocations locations: [CLLocation]
+  ) {
+    let newLocation = locations.last!
+    print("didUpdateLocations \(newLocation)")
+  }
 }
 
