6ce848e71347c1f601ce926b743921468826eb70
Kazimir Makaranka <m2014ky@gmail.com>
2023-08-07 11:55:24 +0300

    Display the address

--- MyLocations/CurrentLocationViewController.swift
+++ MyLocations/CurrentLocationViewController.swift
@@ -46,6 +46,8 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
     } else {
       location = nil
       lastLocationError = nil
+      placemark = nil
+      lastGeocodingError = nil
       startLocationManager()
     }
     updateLabels()
@@ -100,6 +102,7 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
         performingReverseGeocoding = true
         
         geocoder.reverseGeocodeLocation(newLocation) {placemarks, error in
+          self.lastGeocodingError = error
           if error == nil, let places = placemarks, !places.isEmpty {
             self.placemark = places.last!
           } else {
@@ -139,6 +142,15 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
         location.coordinate.longitude)
       tagButton.isHidden = false
       messageLabel.text = ""
+      if let placemark = placemark {
+        addressLabel.text = string(from: placemark)
+      } else if performingReverseGeocoding {
+        addressLabel.text = "Searching for Address..."
+      } else if lastGeocodingError != nil {
+        addressLabel.text = "Error Finding Address"
+      } else {
+        addressLabel.text = "No Address Found"
+      }
     } else {
       latitudeLabel.text = ""
       longitudeLabel.text = ""
@@ -187,6 +199,27 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
     } else {
       getButton.setTitle("Get My Location", for: .normal)
     }
+  }
+  
+  func string(from placemark: CLPlacemark) -> String {
+    var line1 = ""
+    if let tmp = placemark.subThoroughfare {
+      line1 += tmp + " "
+    }
+    if let tmp = placemark.thoroughfare {
+      line1 += tmp
+    }
+    var line2 = ""
+    if let tmp = placemark.locality {
+      line2 += tmp + " "
+    }
+    if let tmp = placemark.administrativeArea {
+      line2 += tmp + " "
+    }
+    if let tmp = placemark.postalCode {
+      line2 += tmp
+    }
+    return line1 + "\n" + line2
   }
 }
 
