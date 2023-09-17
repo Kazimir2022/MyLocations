e030ff69ead93fbeda7a11977a51e08e477e6990
Kazimir Makaranka <m2014ky@gmail.com>
2023-09-17 15:56:53 +0300

    Save the locations.

--- MyLocations/LocationDetailsViewController.swift
+++ MyLocations/LocationDetailsViewController.swift
@@ -28,8 +28,9 @@ class LocationDetailsViewController: UITableViewController {
     latitude: 0,
     longitude: 0)
   var placemark: CLPlacemark?
-  var managedObjectContext: NSManagedObjectContext!
   var categoryName = "No Category"
+  var managedObjectContext: NSManagedObjectContext!
+  var date = Date()
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -43,7 +44,7 @@ class LocationDetailsViewController: UITableViewController {
       addressLabel.text = "No Address Found"
     }
     
-    dateLabel.text = format(date: Date())
+    dateLabel.text = format(date: date)
     
     // Hide keyboard
     let gestureRecognizer = UITapGestureRecognizer(
@@ -64,12 +65,26 @@ class LocationDetailsViewController: UITableViewController {
   // MARK: - Actions
   @IBAction func done() {
     guard let mainView = navigationController?.parent?.view
-    else { return }
+      else { return }
     let hudView = HudView.hud(inView: mainView, animated: true)
     hudView.text = "Tagged"
-    afterDelay(0.6) {
-      hudView.hide()
-      self.navigationController?.popViewController(animated: true)
+
+    let location = Location(context: managedObjectContext)
+    location.locationDescription = descriptionTextView.text
+    location.category = categoryName
+    location.latitude = coordinate.latitude
+    location.longitude = coordinate.longitude
+    location.date = date
+    location.placemark = placemark
+    do {
+      try managedObjectContext.save()
+      afterDelay(0.6) {
+        hudView.hide()
+        self.navigationController?.popViewController(
+          animated: true)
+      }
+    } catch {
+      fatalError("Error: \(error)")
     }
   }
   
