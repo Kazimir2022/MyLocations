f384b7f3e1679c4a3c7382326850829d12f8a9cf
Kazimir Makaranka <m2014ky@gmail.com>
2023-09-17 12:11:24 +0300

    Add the Core Data framework to the app.

---
+++ Location+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Location+CoreDataClass.swift
+//  MyLocations
+//
+//  Created by Kazimir on 17.09.23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Location)
+public class Location: NSManagedObject {
+
+}
