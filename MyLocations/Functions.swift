dc196dbc328a6f7fc9b3f6d8e9ed46a6a71ab511
Kazimir Makaranka <m2014ky@gmail.com>
2023-09-17 15:36:56 +0300

    Troubleshoot Core Data issues.

--- MyLocations/Functions.swift
+++ MyLocations/Functions.swift
@@ -12,3 +12,11 @@ func afterDelay(_ seconds: Double, run: @escaping () -> Void) {
     deadline: .now() + seconds,
     execute: run)
 }
+
+let applicationDocumentsDirectory: URL = {
+  let paths = FileManager.default.urls(
+    for: .documentDirectory,
+       in: .userDomainMask)
+  return paths[0]
+}()
+
