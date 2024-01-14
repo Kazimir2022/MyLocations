e45cca7d41d44636a666a1070110836fbfaf0c93
Kazimir Makaranka <m2014ky@gmail.com>
2024-01-14 15:23:40 +0300

    Add a new Swift source file to the project and name it MapViewController.

---
+++ MyLocations/MapViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MapViewController.swift
+//  MyLocations
+//
+//  Created by Kazimir on 7.01.24.
+//
+import UIKit
+import MapKit
+import CoreData
+
+class MapViewController: UIViewController {
+  @IBOutlet var mapView: MKMapView!
+  
+  var managedObjectContext: NSManagedObjectContext!
+  
+  // MARK: - Actions
+  @IBAction func showUser() {
+    let region = MKCoordinateRegion(
+      center: mapView.userLocation.coordinate,
+      latitudinalMeters: 1000,
+      longitudinalMeters: 1000)
+    mapView.setRegion(
+      mapView.regionThatFits(region),
+      animated: true)
+  }
+  
+  @IBAction func showLocations() {
+  }
+}
+
+extension MapViewController: MKMapViewDelegate {
+}
