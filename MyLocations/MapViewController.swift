65b0e2e2cd0d48181f603925e95e1d52b2a4a3ef
Kazimir Makaranka <m2014ky@gmail.com>
2024-01-15 12:10:51 +0300

    Add pins for locations.

--- MyLocations/MapViewController.swift
+++ MyLocations/MapViewController.swift
@@ -13,6 +13,13 @@ class MapViewController: UIViewController {
   
   var managedObjectContext: NSManagedObjectContext!
   
+  var locations = [Location]()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    updateLocations()
+  }
+  
   // MARK: - Actions
   @IBAction func showUser() {
     let region = MKCoordinateRegion(
@@ -25,6 +32,19 @@ class MapViewController: UIViewController {
   }
   
   @IBAction func showLocations() {
+  }
+  
+  // MARK: - Helper methods
+  func updateLocations() {
+    mapView.removeAnnotations(locations)
+    
+    let entity = Location.entity()
+    
+    let fetchRequest = NSFetchRequest<Location>()
+    fetchRequest.entity = entity
+    
+    locations = try! managedObjectContext.fetch(fetchRequest)
+    mapView.addAnnotations(locations)
   }
 }
 
