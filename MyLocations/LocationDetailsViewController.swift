c3670b7477f1ef3f4fc4816cadcaa46fb529cdd5
Kazimir Makaranka <m2014ky@gmail.com>
2024-02-07 20:41:49 +0300

    Resize table view cell to show image.

--- MyLocations/LocationDetailsViewController.swift
+++ MyLocations/LocationDetailsViewController.swift
@@ -25,7 +25,8 @@ class LocationDetailsViewController: UITableViewController {
   @IBOutlet var dateLabel: UILabel!
   @IBOutlet var imageView: UIImageView!
   @IBOutlet var addPhotoLabel: UILabel!
-  
+  @IBOutlet var imageHeight: NSLayoutConstraint!
+
   var coordinate = CLLocationCoordinate2D(
     latitude: 0,
     longitude: 0)
@@ -175,6 +176,8 @@ class LocationDetailsViewController: UITableViewController {
     imageView.image = image
     imageView.isHidden = false
     addPhotoLabel.text = ""
+    imageHeight.constant = 260
+    tableView.reloadData()
   }
   
   // MARK: - Table View Delegates
